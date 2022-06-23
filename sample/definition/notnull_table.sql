92d01adc0cf2b91eb82f9f89cbdfde363050c39a
telitas <telitas.dev@gmail.com>
2022-06-24 01:00:31 +0900

    Add character length

--- sample/definition/notnull_table.sql
+++ sample/definition/notnull_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE notnull_table (
     character_varying_column CHARACTER VARYING(10) NOT NULL,
     character_large_object_column CHARACTER LARGE OBJECT NOT NULL,
-    binary_varying_column BINARY VARYING NOT NULL,
+    binary_varying_column BINARY VARYING(10) NOT NULL,
     binary_large_object_column BINARY LARGE OBJECT NOT NULL,
     not_supported_type_column NOT SUPPORTED TYPE NOT NULL
 );
